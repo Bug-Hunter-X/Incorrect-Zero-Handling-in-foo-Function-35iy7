5f941a237203233e36fa16ea9cf8d543516536d6
BugHunter-X <askzenai@gmail.com>
2024-12-14 11:56:06 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,8 @@
+function foo(a, b) {
+  return a + b; //This is the correct implementation
+}
+//test cases
+console.log(foo(0, 5)); // Output: 5
+console.log(foo(5, 0)); // Output: 5
+console.log(foo(0, 0)); // Output: 0
+console.log(foo(5, 5)); // Output: 10
